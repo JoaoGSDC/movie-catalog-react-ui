1032764a3d320f7f7229db5ba57e4542d648046f
JoaoGSDC <joaogsdc@gmail.com>
2022-11-25 18:29:26 -0300

    change env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.log(import.meta.env);
+
 const api = axios.create({
-  baseURL: import.meta.env.REACT_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export default api;
